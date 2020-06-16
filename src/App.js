e5afc7fecf5be6800c9b983af5e876a24602f517
Yermakovm <yermakov.m.o@gmail.com>
2020-06-16 23:45:07 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import Navbar from "./Navbar"
-import Form from "./Form"
-import PageContent from "./PageContent"
-import {ThemeProvider} from "./contexts/ThemeContext"
+import React from "react";
+import Navbar from "./Navbar";
+import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
-function App() {
+export default function App() {
   return (
     <ThemeProvider>
-      <PageContent>
-        <Navbar />
-        <Form />
-      </PageContent>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
-
-export default App;
